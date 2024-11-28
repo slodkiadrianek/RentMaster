537a22b26fe97b87f717e20a63acaf6fb39dd879
Adrian Kurek <adikurek11@gmail.com>
2024-11-28 19:51:13 +0100

    database configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/libsql';
+export const db = drizzle(process.env.DB_FILE_NAME!);
 
 export const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
